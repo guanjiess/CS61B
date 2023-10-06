c0d008beb7295b8f7b57b3d97dbe007fdcf10830
guanshixun <guanjie479@gmail.com>
2023-10-06 11:03:05 +0800

    implement commit, not tested

---
+++ proj2/gitlet/CommitTree.java
@@ -0,0 +1,14 @@
+package gitlet;
+
+import java.util.LinkedList;
+
+public class CommitNode {
+    private String name;
+    private String hash;
+
+    public CommitNode(String name, String hash){
+        this.name = name;
+        this.hash = hash;
+    }
+
+}
